316c980d36783f743185a42bed33cc8ab572b961
Scotty Vernon <scott.vernon@gmail.com>
2015-01-15 19:33:32 +0000

    Flesh out modules and states used on homepage.

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,19 @@ var plumber = require('gulp-plumber');
 var prefix = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+var gutil = require('gulp-util');
 
-// changeEvent = function(evt) {
-//     gutil.log('File', gutil.colors.cyan(evt.path.replace(new RegExp('/.*(?=/' + basePaths.src + ')/'), '')), 'was', gutil.colors.magenta(evt.type));
-// };
+var basePaths = {
+  src:    'app/assets/',
+  dest:   'app/static/'
+};
+
+changeEvent = function(evt) {
+  gutil.log('File', gutil.colors.cyan(evt.path.replace(new RegExp('/.*(?=/' + basePaths.src + ')/'), '')), 'was', gutil.colors.magenta(evt.type));
+};
 
 gulp.task('sass', function () {
-  gulp.src('./assets/_scss/*.scss')
+  gulp.src('./assets/_scss/main.scss')
 
     //Plumb pipe breaks incase of errors
     .pipe(plumber())
@@ -18,8 +24,8 @@ gulp.task('sass', function () {
 
     //Autoprefixer
     .pipe(prefix({
-        browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'],
-        cascade: false
+      browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'],
+      cascade: false
     }))
 
     .pipe(gulp.dest('./assets/stylesheets'));
@@ -27,8 +33,8 @@ gulp.task('sass', function () {
 
 
 //Watch task built for speed in development
-gulp.task('sass-watch', function() {
-  watch('./assets/_scss/**/*.scss')
+gulp.task('sass-dev', function() {
+  gulp.src('./assets/_scss/main.scss')
 
     //Plumb pipe breaks incase of errors
     .pipe(plumber())
@@ -38,8 +44,8 @@ gulp.task('sass-watch', function() {
     .pipe(gulp.dest('./assets/stylesheets'));
 });
 
-// gulp.task('sass-watch', ['sass'], function(){
-//   gulp.watch('./assets/_scss/*.scss', ['sass']).on('change', function(evt) {
-//       changeEvent(evt);
-//   });
-// });
+gulp.task('sass-watch', ['sass-dev'], function(){
+  gulp.watch('./assets/_scss/**/*.scss', ['sass-dev']).on('change', function(evt) {
+    changeEvent(evt);
+  });
+});
