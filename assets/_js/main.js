dc41a1f04dd56247ba2a706147f3ae54a1070cab
Scotty Vernon <scott.vernon@gmail.com>
2015-01-16 22:14:00 +0000

    Disqus vertial rhythm part 1.

--- assets/_js/main.js
+++ assets/_js/main.js
@@ -10,6 +10,35 @@ $(function() {
   // $(window).load(function () {
 
     window.onload = function() {
+
+      // var $disqus = $('#disqus_thread');
+      // var height = $disqus.height();
+
+      // var baseline = 16;
+      // var remainder = height % baseline;
+
+      // if !(remainder) {
+      //   $disqus.css({
+      //     'padding-bottom': remainder + "px"
+      //   });
+      // }
+
+      var editable = true; // set a flag
+      var disqusInitTest = setInterval(function() {
+      // Initially Disqus renders this div with the height of 0px prior to the comments being loaded. So run a check to see if the comments have been loaded yet.
+      console.log('interval');
+
+        var disqusHeight = $('#disqus_thread').height();
+        if ( disqusHeight > 0 ) {
+          if (editable) { // To make sure that the changes you want to make only happen once check to see if the flag has been changed, if not run the changes and update the flag.
+            editable = false;
+            clearInterval(disqusInitTest);
+            console.log('hello? disqus loaded');
+            // Your code here...
+          }
+        }
+      }, 100);
+
       // $('img').rhythm();
       // if ($('.media-window img').length !== 0) {
       //   var url = $('.media-window img').attr('src');
