97120b07f809fa7c53913a01b2477f86b89fdd02
Scotty Vernon <scott.vernon@gmail.com>
2015-01-17 15:26:07 +0000

    Refactor container behaviour. Sort header and footer out. Vertical rhythm for disqus part 2.

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1596,7 +1596,7 @@ window.$ === undefined && (window.$ = Zepto)
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],2:[function(require,module,exports){
-require('zepto');
+;require('zepto');
 // require('./rhythm');
 
 $(function() {
@@ -1609,33 +1609,50 @@ $(function() {
 
     window.onload = function() {
 
-      // var $disqus = $('#disqus_thread');
+      var $disqus = $('#disqus_thread');
       // var height = $disqus.height();
 
-      // var baseline = 16;
-      // var remainder = height % baseline;
+      function fixRhythm(height) {
+        // var baseline = 16;
+        // var remainder = height % baseline;
 
-      // if !(remainder) {
-      //   $disqus.css({
-      //     'padding-bottom': remainder + "px"
-      //   });
-      // }
+        var body_font_size_px   = parseFloat($('body').css('font-size'));
+        var body_line_height_px = parseFloat($('body').css('line-height'));
+        var body_line_height_rem = body_line_height_px / body_font_size_px;
+
+        var baseline = body_line_height_px / 2;
+        var remainder = height % baseline;
+        var invertRemainder = baseline - remainder;
+
+        console.log('Line height:', baseline);
+        console.log('Disqus height:', height);
+        console.log('Remainder:', invertRemainder);
+
+        if (remainder > 0) {
+          $disqus.css({
+            'padding-bottom': invertRemainder + "px"
+          });
+          console.log('Fixing height.');
+        }
+
+      }
 
       var editable = true; // set a flag
       var disqusInitTest = setInterval(function() {
       // Initially Disqus renders this div with the height of 0px prior to the comments being loaded. So run a check to see if the comments have been loaded yet.
-      console.log('interval');
+      // console.log('interval');
 
-        var disqusHeight = $('#disqus_thread').height();
+        var disqusHeight = $disqus.height();
         if ( disqusHeight > 0 ) {
           if (editable) { // To make sure that the changes you want to make only happen once check to see if the flag has been changed, if not run the changes and update the flag.
             editable = false;
             clearInterval(disqusInitTest);
-            console.log('hello? disqus loaded');
+            // console.log('hello? disqus loaded');
             // Your code here...
+            fixRhythm(disqusHeight);
           }
         }
-      }, 100);
+      }, 1000);
 
       // $('img').rhythm();
       // if ($('.media-window img').length !== 0) {
