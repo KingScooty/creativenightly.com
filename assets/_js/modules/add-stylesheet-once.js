a6fed8436fd4da75faf3e7ce566434fe15aa965d
Scotty Vernon <scott.vernon@gmail.com>
2015-02-01 01:12:49 +0000

    Add ajax gist code.

---
+++ assets/_js/modules/add-stylesheet-once.js
@@ -0,0 +1,7 @@
+function add_stylesheet_once( url ){
+  $head = $('head');
+  if( $head.find('link[rel="stylesheet"][href="'+url+'"]').length < 1 )
+    $head.append('<link rel="stylesheet" href="'+ url +'" type="text/css" />');
+}
+
+module.exports = add_stylesheet_once;
